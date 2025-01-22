ea82c32038b049c52dd33d1f73950dc993ba9ef6
George(chiyum) <pray870712@gmail.com>
2025-01-22 11:37:54 +0800

    feat: 🎸 波浪動畫

--- src/layouts/layout-default.vue
+++ src/layouts/layout-default.vue
@@ -3,6 +3,7 @@
 
 // const { t } = useI18n();
 // const author = import.meta.env.VITE_PROJECT_AUTHOR;
+import ArchAnimation from "@/components/arch-animation.vue";
 </script>
 
 <template>
@@ -10,17 +11,25 @@
     <div class="layout-default-head">
       <a class="layout-default-head-frontPage">a</a>
       <div class="layout-default-linkBar">
-        <a class="layout-default-link">Portfolio</a>
-        <a class="layout-default-link">About Me</a>
-        <a class="layout-default-link">Blog</a>
-        <a class="layout-default-link"
-          >Link
-          <img
-            class="layout-default-img"
-            src="../assets/images/icon/link.png"
-            alt=""
-          />
-        </a>
+        <arch-animation color="#ffffff">
+          <a class="layout-default-link"> Portfolio </a>
+        </arch-animation>
+        <arch-animation color="#ffffff">
+          <a class="layout-default-link">About Me</a>
+        </arch-animation>
+        <arch-animation color="#ffffff">
+          <a class="layout-default-link">Blog</a>
+        </arch-animation>
+        <arch-animation direction="top" color="#ffffff">
+          <a class="layout-default-link"
+            >Link
+            <img
+              class="layout-default-img"
+              src="../assets/images/icon/link.png"
+              alt=""
+            />
+          </a>
+        </arch-animation>
       </div>
       <div class="layout-default-connect">
         <a href="" class="layout-default-link"
