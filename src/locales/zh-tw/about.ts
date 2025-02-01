e8bbf58879fcb17df895d6fee5dbfea62c03ea68
BaGuai8 <vuxups70115@gmail.com>
2025-02-01 17:37:49 +0800

    fix: 🐛 修改版型第三版

--- src/locales/zh-tw/about.ts
+++ src/locales/zh-tw/about.ts
@@ -5,13 +5,14 @@ export default {
   openingText: " 準備好幫助你打造前所未有的體驗！",
   button: "聯絡我",
   about: "Happy to see you",
-  "introductionCard.title": "Movie List",
+  "introductionCard.title": "電影清單",
   "introductionCard.narrative": "我的人生電影清單",
   "introductionCard.connect": "這些電影都曾經在某一刻接住我,也推薦給你們！",
   "introductionCard.narrative.music": "這是我正在聆聽的音樂",
   "introductionCard.connect.music":
     "這些是我在設計線框圖時會聽的歌曲。我也很樂意發掘更多好音樂，懂我意思吧 😉？",
   "introductionCard.title.music": "我的播放清單",
+  "introductionCard.title.game": "遊戲",
   "introductionCard.narrative.game": "這是我正在玩的遊戲",
   "introductionCard.connect.game":
     "寫code之餘也會打打遊戲!戰旗聯盟目前卡在金牌上不去T_T",
